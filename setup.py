0003945587e17aebe4a3fec79855ca46eaed5b0a
Maxence Lannuzel <maxence.lannuzel@gmail.com>
2021-07-05 10:26:36 +0200

    Updating version.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='vdr',
-    version='1.0.6',
+    version='1.0.7',
     author='Maxence Lannuzel',
     author_email='maxence.lannuzel@ecole-navale.fr',
     description='A simple library to simulate a VDR.',
